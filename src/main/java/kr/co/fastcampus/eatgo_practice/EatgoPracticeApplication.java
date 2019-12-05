2f0550d71829619d38a74cb8bf3acebbab42f275
KwakCena <rhkrgudwh@gmail.com>
2019-12-05 10:55:27 +0900

    first commit

---
+++ src/main/java/kr/co/fastcampus/eatgo_practice/EatgoPracticeApplication.java
@@ -0,0 +1,13 @@
+package kr.co.fastcampus.eatgo_practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EatgoPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EatgoPracticeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kr/co/fastcampus/eatgo_practice/EatgoPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.fastcampus.eatgo_practice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EatgoPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
