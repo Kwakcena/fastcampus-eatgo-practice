5fa280b590ca83a3cb5b02177381895f8d5dfd39
KwakCena <rhkrgudwh@gmail.com>
2019-12-06 21:33:03 +0900

    가게 상세-2 (상세 목록을 찾는 효과적인 방법)
    레스토랑의 목록이 엄청나게 많다면 어떻게 해야 할까?
    이럴경우 굉장히 효과적인 탐색 방법이 있다면 어떻게 해야 할까?
    지금은 컨트롤러에서 repository 안에 있는 어떤 내용들을 가져와 통째로 검색하고 있다.
    따라서 이는 컨트롤러가 해야하는 일이 아니다.
    
    id를 통해 하나의 레스토랑을 찾는 행위도 repository에서 하도록 해보자.
    
    id를 통해 restaurant을 찾는 findById 메서드를 구현한다. 여기에는 restaurants의
    전체 목록이 사용되므로 이를 객체의 필드로 옮겨 findAll과 findById 두 곳에서
    사용가능하도록 한다.
    
    기존의 restaurant controller는 굉장히 길고 복잡했는데, repository를 이용함으로써
    굉장히 단순한 형태로 바뀌게 되었다. UI Layer는 사용자와 내부에 있는 우리 비즈니스
    로직 또는 도메인 모델들이 서로 상관없도록, 중간 징검다리가 되도록 코드가 들어가게
    되고, 실제로 이를 활용하는 것들은 전부 도메인 모델 쪽에 있게 된다. 그래서 컨트롤러는
    굉장히 단순하고 처리해야 하는 부분은 domain layer에서 하는 것이다.
    
    이렇게 둘을 나누었기 때문에 우리는 얼마든지 처리에 대한 부분을 다른것으로 바꿀 수
    있다. 지금은 자바의 리스트를 사용하지만, DB를 활용하여 더 많은 가게 목록을
    활용한다던지, 데이터 베이스를 활용함으로써 여러 프로그램에서 데이터를 사용하도록
    바꿀 수 있다.

--- eatgo-api/src/main/java/kr/co/fastcampus/eatgo_practice/interfaces/RestaurantController.java
+++ eatgo-api/src/main/java/kr/co/fastcampus/eatgo_practice/interfaces/RestaurantController.java
@@ -22,16 +22,7 @@ public class RestaurantController {
 
     @GetMapping("/restaurants/{id}")
     public Restaurant detail(@PathVariable("id") Long id) {
-        List<Restaurant> restaurants = new ArrayList<>();
-
-        restaurants.add(new Restaurant(1004L, "Bob zip", "Seoul"));
-        restaurants.add(new Restaurant(2020L, "Cyber Food", "Seoul"));
-
-        Restaurant restaurant = restaurants.stream()
-                .filter(r -> r.getId().equals(id))
-                .findFirst()
-                .orElse(null);
-
+        Restaurant restaurant = repository.findById(id);
         return restaurant;
     }
 }
